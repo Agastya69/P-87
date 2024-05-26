2c3a4052be739d5f770ab96670b364a995689ccb
Agastya69 <145190878+Agastya69@users.noreply.github.com>
2024-05-26 16:01:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ block_image_object=Img;
 block_image_object.scaleToWidth(block_image_width);
 block_image_object.scaleToHeight(block_image_height);
 block_image_object.set({
-	top:block_y
+	top:block_y,
+	left:block_x
 });
 canvas.add(block_image_object);
 });
